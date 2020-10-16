a4673b8bf05e5b2dc16f04a4b92136577db1f4f4
Hamed Issam <hamedissam@gmail.com>
2020-10-16 21:03:02 +0100

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports={
+module.exports = {
     mode: 'development',
     entry: {
         'chart': './src/index.js',
-        'chart.min': './src/index.js',
     },
     output: {
         library: 'IssChart',
@@ -18,36 +16,31 @@ module.exports={
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
     },
-    module: { 
-        rules:[
-            {
+    module: {
+        rules: [{
                 test: /\\.(js|jsx)$/,
                 include: [
-                  path.resolve(__dirname, 'src')
+                    path.resolve(__dirname, 'src')
                 ],
                 loader: 'babel-loader',
-              },
+            },
             {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader'],
             },
-        ] 
+        ]
     },
-    plugins:[
-        new uglifyJsPlugin({
-            include: /\.min\.js$/,
-          }),
+    plugins: [
         new webpack.ProgressPlugin(),
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, 'index.html'),
-            excludeChunks: ["chart.min"],
             inject: 'head'
         }),
         new webpack.HotModuleReplacementPlugin(),
         new CopyPlugin({
             patterns: [
-              { from: 'src/style.css', to: 'src/style.css' },
+                { from: 'src/style.css', to: 'src/style.css' },
             ],
-          }),
+        }),
     ]
 }
